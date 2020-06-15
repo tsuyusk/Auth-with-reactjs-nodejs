99037673d45a4018597ad0074cb22c6902fcf68b
tsuyan <rafaeldesenhos99@gmail.com>
2020-06-15 11:12:02 +0000

    changed baseurl of axios create

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://3333-ebfc09a3-0a2a-47fd-bc02-c757fbaefc2a.ws-us02.gitpod.io/"
-})
+  baseURL: "https://localhost:3333/"
+});
 
 export default api;
